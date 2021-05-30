86f5aeec4383072b3e384b5cc50fa79c80ee34d9
Fatlind Bytyqi <fatlind-bytyqi@outlook.com>
2021-05-30 18:54:29 +0200

    Change LibriController

---
+++ BiblotekaWeb/Areas/admin/Data/ILibriService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BiblotekaWeb.Areas.admin.Models;
+
+namespace BiblotekaWeb.Areas.admin.Data
+{
+    public interface ILibriService
+    {
+        List<Libri> GetAllLibri();
+        void ShtoLibrin(Libri libri);
+        Libri GetLibriById(string id);
+        void PerditesoLibrin(Libri libri);
+        void DeleteLibrin(string id);
+    }
+}
---
+++ BiblotekaWeb/Areas/admin/Data/LibriService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BiblotekaWeb.Areas.admin.Models;
+
+namespace BiblotekaWeb.Areas.admin.Data
+{
+    public class LibriService : ILibriService
+    {
+        private readonly BiblotekaWebContext _context;
+
+        public LibriService(BiblotekaWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Libri> GetAllLibri()
+        {
+
+            return _context.Libris.ToList();
+        }
+
+        public void ShtoLibrin(Libri libri)
+        {
+            _context.Libris.AddAsync(libri);
+            _context.SaveChangesAsync();
+        }
+
+        public Libri GetLibriById(string id)
+        {
+            return _context.Libris.FirstOrDefault(x => x.LibriId == id);
+        }
+
+        public void PerditesoLibrin(Libri libri)
+        {
+            _context.Libris.Update(libri);
+            _context.SaveChangesAsync();
+        }
+
+        public void DeleteLibrin(string id)
+        {
+            var libri = _context.Libris.FirstOrDefault(x => x.LibriId == id);
+            if (libri != null) _context.Libris.Remove(libri);
+        }
+    }
+}
