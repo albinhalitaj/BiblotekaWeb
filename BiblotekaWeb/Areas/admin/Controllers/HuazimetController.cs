eba637f1a90ef1c3d981311e7f57b12f37323b30
Albin Halitaj <albinhalitaj2019@hotmail.com>
2021-05-31 20:48:42 +0200

    updated huazimet

--- BiblotekaWeb/Areas/admin/Controllers/HuazimetController.cs
+++ BiblotekaWeb/Areas/admin/Controllers/HuazimetController.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using BiblotekaWeb.Areas.admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BiblotekaWeb.Areas.admin.Controllers
 {
     [Area("admin")]
     public class HuazimetController : Controller
     {
+        private readonly BiblotekaWebContext _context;
+
+        public HuazimetController(BiblotekaWebContext context)
+        {
+            _context = context;
+        }
+        
         public IActionResult Index()
         {
             return View();
@@ -17,6 +26,13 @@ namespace BiblotekaWeb.Areas.admin.Controllers
         [HttpGet]
         public IActionResult Huazo()
         {
+            ViewBag.Librat = _context.Libris.Where(x => x.NumriKopjeve > 0).ToList();
+            var list = _context.Klientis.Select(s => new SelectListItem
+            {
+                Value = s.KlientiId,
+                Text = string.Concat(s.Emri, " ", s.Mbiemri)
+            }).ToList();
+            ViewBag.Klientet = new SelectList(list, "Value", "Text");
             return View();
         }
 
