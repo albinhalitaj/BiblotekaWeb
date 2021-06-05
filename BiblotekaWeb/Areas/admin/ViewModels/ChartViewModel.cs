31f0bc826a993f8eb155b7e161e48bcfb8af9ccf
Albin Halitaj <albinhalitaj2019@hotmail.com>
2021-06-06 01:07:37 +0200

    init

---
+++ BiblotekaWeb/Areas/admin/ViewModels/ChartViewModel.cs
@@ -0,0 +1,11 @@
+using DocumentFormat.OpenXml.CustomProperties;
+using FluentEmail.Core.Models;
+
+namespace BiblotekaWeb.Areas.admin.ViewModels
+{
+    public class ChartViewModel
+    {
+        public string Muaji { get; set; }
+        public int Numri { get; set; }
+    }
+}
